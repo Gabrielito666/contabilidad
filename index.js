bed260587630f2f6387fff7d40c5dea559a6a84a
Anto <aemora@uc.cl>
2023-08-04 19:03:45 -0400

    estructura de la pagina funcionando

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const db = require('./modulos/db');
 app.use(express.json());
 
 app.post('/crearCuenta', async(req, res)=>{res.json(await db.crearCuenta(req.body.nombre, req.body.usuarios))});
-app.post('/agragarMovimiento', async(req, res)=>{res.json(await db.agregarMovimiento(req.body.cuenta, req.body.movimiento))});
+app.post('/agregarMovimiento', async(req, res)=>{res.json(await db.agregarMovimiento(req.body.cuenta, req.body.movimiento))});
 app.post('/listadoCuentas', async(req, res)=>{res.json({cuentas : await db.listadoCuentas(req.body.usuario)})});
 app.post('/detalleCuenta', async(req, res)=>{res.json({movimeintos : await db.detalleCuenta(req.body.cuenta)})});
--- public/script.js
+++ public/script.js
@@ -1,14 +1,58 @@
+const botonX = document.querySelectorAll('.exit');
+const seccionOculta = document.getElementById('section_hidden');
+const listadoCuentas = document.getElementById('listado_cuentas');
+const ventanaDetalleCuenta = document.getElementById('div_cuenta');
+const btnAgregarCuenta = document.getElementById('btn_agregar_cuenta');
+const ventanaCrearCuenta = document.getElementById('div_agregar_cuenta');
+const btnAgregarMovimiento = document.getElementById('btn_agregar_movimiento');
+const ventanaAgregarMovimiento = document.getElementById('div_agregar_movimiento');
+
+botonX.forEach(boton => {
+  boton.addEventListener('click', function () {
+    seccionOculta.style.display = 'none';
+  })
+})
+
+btnAgregarCuenta.addEventListener('click', () => {
+  mostrarVentana(ventanaCrearCuenta);
+})
+
+btnAgregarMovimiento.addEventListener('click', () => {
+  mostrarVentana(ventanaAgregarMovimiento);
+})
+
+function mostrarVentana (ventana) {
+  seccionOculta.style.display = 'flex';
+  ventanaCrearCuenta.style.display = 'none';
+  ventanaDetalleCuenta.style.display = 'none';
+  ventanaAgregarMovimiento.style.display = 'none';
+  ventana.style.display = 'flex';
+}
 
 
 
+function renderizarLista(arr){
+  //INNERHTML al div listadoCuentas basado en el array que se obtiene como parametro. <h2 class="cuenta">Banco de Chile</h2>
+  let listado = '';
+  arr.forEach(indice=> {
+    listado += h2('cuenta', indice);
+  })
+  listadoCuentas.innerHTML = listado;
+  const cuentas = document.querySelectorAll('.cuenta');
+  cuentas.forEach(cuenta => {
+    cuenta.addEventListener('click', () => {
+     mostrarVentana(ventanaDetalleCuenta);
+    })
+  })
+}
 
-
+renderizarLista(['Banco de Chile', 'Banco Estado, cuenta RUT', 'Banco Estado, cuenta de ahorro', 'Cuenta familiar']);
 
 
 function tr(arr){return `<tr>${arr.map(x=>td(x)).join('')}</tr>`};
 function td(x){return `<td>${x}</td>`};
 function btn(c, n, i, t, disabled){return `<button class="${c}" name="${n}" id="${i}" ${disabled ? 'disabled' : ''}>${t}</button>`}
-
+function h2(c, t){return `<h2 class="${c}">${t}</h2>`};
 function ajaxPost (url, objeto, callback){
   let xhr = new XMLHttpRequest();
   xhr.open('POST', url, true);
@@ -18,4 +62,3 @@ function ajaxPost (url, objeto, callback){
   };
   xhr.send(JSON.stringify(objeto));
 };
-ajaxPost('/me', undefined, gestorNoticias);
