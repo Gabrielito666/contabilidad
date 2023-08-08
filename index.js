d6f6e75a30b0a17d1aa1182fdcd534ea3a9aa029
Gabrielito666 <galileo.fandango@gmail.com>
2023-08-08 19:07:36 -0400

    mas pruebas

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(session({
 app.use('/', router);
 
 router.get('/', async(req, res)=>{res.redirect('/contabilidad/app/index.html')})
-router.post('/login', async(req, res)=>{if(db.login(req.usuario, req.password)){req.session.usuario = req.body.usuario; res.redirect('/../app/index.html')}else{res.redirect(`?${req.usuario}`)}})
+router.post('/login', async(req, res)=>{if(db.login(req.body.usuario, req.body.password)){req.session.usuario = req.body.usuario; res.redirect('/../app/index.html')}else{res.redirect(`?${req.usuario}`)}})
 router.post('/crearCuenta', async(req, res)=>{res.json(await db.crearCuenta(req.body.nombre, req.body.usuarios))});
 router.post('/agregarMovimiento', async(req, res)=>{res.json(await db.agregarMovimiento(req.body.cuenta, req.body.movimiento))});
 router.post('/listadoCuentas', async(req, res)=>{res.json({cuentas : await db.listadoCuentas('Anto'), usuario : 'Anto'})});
