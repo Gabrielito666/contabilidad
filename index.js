783c8f6828049fbb55419814246aac6dc5b12402
Gabrielito666 <galileo.fandango@gmail.com>
2023-08-08 18:54:47 -0400

    arreglo de rutas

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/', router);
 
+app.get('/', 'contabilidad/app/index.html')
 router.post('/login', async(req, res)=>{if(db.login(req.usuario, req.password)){req.session.usuario = req.body.usuario; res.redirect('/../app/index.html')}else{res.redirect(`?${req.usuario}`)}})
 router.post('/crearCuenta', async(req, res)=>{res.json(await db.crearCuenta(req.body.nombre, req.body.usuarios))});
 router.post('/agregarMovimiento', async(req, res)=>{res.json(await db.agregarMovimiento(req.body.cuenta, req.body.movimiento))});
